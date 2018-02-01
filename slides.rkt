4ab0e2f89ffc4e6100e3dfc0b27efb2ce846c889
Marcos Minond <minond.marcos@gmail.com>
2018-01-31 22:53:58 -0700

    yet again, cleaning up the abstract

--- slides.rkt
+++ slides.rkt
@@ -10,5 +10,5 @@
 
 (slide
   #:title "Abstract"
-  (para "An understanding of program evaluation can be a truly enlightening thing for a programmer. By breaking down the process to its essence and implement the parsing and interpretation steps for a Lisp, we will explore both the interesting and oddly simple algorithms employed in making a language.")
+  (para "An understanding of program evaluation can be a truly enlightening thing for a programmer. By breaking down the process to its essence and implementing the parsing and interpretation steps for a Lisp, we will explore both the interesting and oddly simple algorithms employed in making a language.")
   (para "Our Lisp will have familiar characteristics like variables, higher-order functions, conditionals, and lexical scope."))
