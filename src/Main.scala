8cb3edfa9d9414d37716b4aab943600c177a6b89
Marcos Minond <minond.marcos@gmail.com>
2018-03-18 19:38:05 -0600

    working scanner

--- src/Main.scala
+++ src/Main.scala
@@ -2,7 +2,10 @@ package xyz.minond.talk.pti
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val scanner = new Scanner("(define x 123)")
-    scanner foreach println
+    new Scanner("""(define x 123)""") foreach println
+    new Scanner("""(define y "hi here")""") foreach println
+    new Scanner("""     (define x 123)   """) foreach println
+    new Scanner(""""one two three"four 1 2 3 123""") foreach println
+    new Scanner("""one->two->three""") foreach println
   }
 }
--- src/Scanner.scala
+++ src/Scanner.scala
@@ -3,20 +3,47 @@ package xyz.minond.talk.pti
 class Scanner(raw: String) extends Iterator[Token] {
   import Token._
 
-  val src = raw.toList.toIterator
-
-  def hasNext(): Boolean = {
-    src.hasNext
-  }
+  val src = raw.trim.toList.toIterator.buffered
 
   def next(): Token = {
     src.next match {
-      case '(' => Token(OPEN_PAREN, None)
-      case ')' => Token(CLOSE_PAREN, None)
-      case 47  => Token(SQUOTE, None)
-      case '#' => Token(POUND, None)
-      case '"' => Token(IDENTIFIER, None)
-      case _   => Token(IDENTIFIER, None)
+      case ' '  => next
+      case '('  => Token(OPEN_PAREN, None)
+      case ')'  => Token(CLOSE_PAREN, None)
+      case '#'  => Token(POUND, None)
+      case '\'' => Token(SQUOTE, None)
+
+      case '"' =>
+        // XXX This should handle escaped quotes
+        val str = src.takeWhile(not(is('"')))
+        Token(STRING, Some(str.mkString))
+
+      case n if isDigit(n) =>
+        // XXX Should validate number here
+        val nums = n :: src.takeWhile(or(isDigit, is('.'))).toList
+        Token(NUMBER, Some(nums.mkString))
+
+      case h =>
+        val chars = h :: src.takeWhile(isIdentifier).toList
+        Token(IDENTIFIER, Some(chars.mkString))
     }
   }
+
+  def hasNext(): Boolean =
+    src.hasNext
+
+  def isDigit(c: Char): Boolean =
+    c >= '0' && c <= '9'
+
+  def isIdentifier(c: Char): Boolean =
+    c != '(' && c != ')' && c != ' '
+
+  def is(c: Char) =
+    (x: Char) => c == x
+
+  def not(f: (Char) => Boolean) =
+    (x: Char) => !f(x)
+
+  def or(f1: (Char) => Boolean, f2: (Char) => Boolean) =
+    (x: Char) => f1(x) || f2(x)
 }
