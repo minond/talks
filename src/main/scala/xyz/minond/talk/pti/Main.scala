077d3e2e30e1eb709b6cd9b3b26945e07e834a3f
Marcos Minond <minond.marcos@gmail.com>
2018-03-21 00:58:32 -0600

    updating samples

--- src/main/scala/xyz/minond/talk/pti/Main.scala
+++ src/main/scala/xyz/minond/talk/pti/Main.scala
@@ -2,5 +2,25 @@ package xyz.minond.talk.pti
 
 object Main {
   def main(args: Array[String]): Unit =
-    new Parser(new Tokenizer("(+ 1 2)")) foreach println
+    new Parser(new Tokenizer("""
+
+(define TRUE #t)
+
+(define             one                 1)
+
+(
+
+define
+two
+2
+
+)
+
+(+ 1 2)
+
+(define x*2
+  (lambda (x)
+    (* x 2)))
+
+      """)) foreach println
 }
