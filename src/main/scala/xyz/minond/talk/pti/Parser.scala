b6dcfb2d3d4cd2146b70f4989c8a344505c7b2c9
Marcos Minond <minond.marcos@gmail.com>
2018-03-20 21:42:42 -0600

    parser wip

--- src/main/scala/xyz/minond/talk/pti/Parser.scala
+++ src/main/scala/xyz/minond/talk/pti/Parser.scala
@@ -1,6 +1,22 @@
 package xyz.minond.talk.pti
 
 import Statement._
+import Token.{IDENTIFIER, POUND}
+
+object Parser {
+  object Error {
+    val STR_INVALID_BOOL = "Cannot parse boolean value."
+    def STR_INVALID_BOOL_VALUE(value: String) =
+      s"Expecting either 'f' or 't' but found ${value} instead."
+    def STR_INVALID_BOOL_TOK(token: Token) =
+      s"Expecting either 'f' or 't' but found ${token} instead."
+
+    val STR_INVALID_TOK = "Cannot parse invalid token."
+    def STR_UNEXPECTED_TOK(id: Token.Id) = s"Found unexpected token: ${id}"
+    def STR_EXPECTING_ONE_OF(ids: Token.Id*) =
+      s"Expecting (one of): ${ids.mkString(", ")}."
+  }
+}
 
 /*
  * Grammar:
@@ -17,12 +33,56 @@ class Parser(source: Tokenizer) extends Iterator[Either[Error, Statement]] {
   def hasNext(): Boolean =
     tokens.hasNext
 
-  def next(): Either[Error, Statement] =
-    ???
+  def next(): Either[Error, Statement] = {
+    tokens.head match {
+      case Right(Token(POUND, _)) => parseBoolean
 
-  def ok(stmt: Statement) =
-    Right(stmt)
+      case Right(_) => ???
 
-  def err(message: String, stmt: Option[Statement] = None) =
-    Left(Error(message, stmt))
+      case Left(Token.Error(msg, _)) =>
+        Left(Error(Parser.Error.STR_INVALID_TOK, Some(Error(msg))))
+    }
+  }
+
+  def expect(ids: Token.Id*): Either[Error, Token] = {
+    tokens.head match {
+      case Right(token) if ids contains token.id =>
+        tokens.next
+        Right(token)
+
+      case Left(Token.Error(msg, _)) =>
+        Left(Error(Parser.Error.STR_INVALID_TOK, Some(Error(msg))))
+
+      case Right(Token(id, _)) =>
+        Left(
+          Error(Parser.Error.STR_UNEXPECTED_TOK(id),
+                Some(Error(Parser.Error.STR_EXPECTING_ONE_OF(ids: _*)))))
+    }
+  }
+
+  def parseBoolean() = {
+    expect(POUND) match {
+      case Left(err) =>
+        Left(Error(Parser.Error.STR_INVALID_BOOL, Some(err)))
+
+      case Right(_) =>
+        expect(IDENTIFIER) match {
+          case Right(Token(IDENTIFIER, Some("t"))) => Right(BooleanStmt(true))
+          case Right(Token(IDENTIFIER, Some("f"))) => Right(BooleanStmt(false))
+
+          case Right(Token(IDENTIFIER, Some(invalid))) =>
+            Left(
+              Error(Parser.Error.STR_INVALID_BOOL,
+                    Some(Error(Parser.Error.STR_INVALID_BOOL_VALUE(invalid)))))
+
+          case Right(token) =>
+            Left(
+              Error(Parser.Error.STR_INVALID_BOOL,
+                    Some(Error(Parser.Error.STR_INVALID_BOOL_TOK(token)))))
+
+          case Left(err) =>
+            Left(Error(Parser.Error.STR_INVALID_BOOL, Some(err)))
+        }
+    }
+  }
 }
