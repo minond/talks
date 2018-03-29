4fddf1d001233cebb910919c3ca7e89948f70552
Marcos Minond <minond.marcos@gmail.com>
2018-03-29 14:27:34 -0600

    playing with code type setting

--- presentation.rkt
+++ presentation.rkt
@@ -1,10 +1,16 @@
 #lang slideshow
 
+(require slideshow/code)
+
 (current-main-font "Baskerville")
+(current-code-font "IBM Plex Mono")
+(code-colorize-enabled #f)
+
+(define (mono code)
+  (text code (list* (current-code-font)) (current-font-size)))
 
 (slide
   #:layout 'center
-
   (titlet "From parsing to interpretation")
   (t "Let's build a language"))
 
@@ -12,3 +18,26 @@
   #:title "Abstract"
   (para "An understanding of program evaluation can be a truly enlightening thing for a programmer. By breaking down the process to its essence and implementing the parsing and interpretation steps for a Lisp, we will explore both the interesting and oddly simple algorithms employed in making a language.")
   (para "Our Lisp will have familiar characteristics like variables, higher-order functions, conditionals, and lexical scope."))
+
+(slide
+  #:title "Code"
+  (code
+    (define ->
+      (lambda :lazy (arg . fns)
+        (cond
+          ((null? fns) arg)
+          (#t (->lambda arg fns)))))))
+
+(slide
+  #:title "Code"
+  (code
+    _import java.nio.charset.Charset
+    _import java.nio.file.{Files, Paths}
+    _import scala.util.{Try, Failure, Success}))
+
+(slide
+  #:title "Code"
+
+  (mono
+    (string-join '("one"
+                   "two"))))
