65868fd3bb8f9f49d6efaeda9c3c146958b3f290
Marcos Minond <minond.marcos@gmail.com>
2018-06-02 20:51:13 -0600

    talk talk talk

--- presentation.rkt
+++ presentation.rkt
@@ -30,21 +30,21 @@
 (current-code-font "IBM Plex Mono")
 (code-colorize-enabled #f)
 
-; (define (unmargin line)
-;   (let ([parts (string-split line "|")])
-;     (string-trim (if (= 1 (length parts))
-;                    (car parts)
-;                    (car (cdr parts))))))
-;
-; (define (mono texts)
-;   (apply vc-append 1
-;          (for/list ([line (string-split (string-trim texts) "\n")])
-;            (with-size ((get-current-code-font-size))
-;                       (with-font (current-code-font)
-;                                  (para #:fill? #t
-;                                        #:width (* 1.1 (current-para-width))
-;                                        #:align 'left
-;                                        line))))))
+(define (unmargin line)
+  (let ([parts (string-split line "|")])
+    (string-trim (if (= 1 (length parts))
+                   (car parts)
+                   (car (cdr parts))))))
+
+(define (mono texts)
+  (apply vc-append 1
+         (for/list ([line (string-split (string-trim texts) "\n")])
+           (with-size ((get-current-code-font-size))
+                      (with-font (current-code-font)
+                                 (para #:fill? #t
+                                       #:width (* 1.1 (current-para-width))
+                                       #:align 'left
+                                       line))))))
 
 (define (p txt)
   (para (string-normalize-spaces txt)))
@@ -54,48 +54,75 @@
   (titlet "From parsing to interpretation")
   (t "Let's build a language"))
 
-; (slide
-;   #:title "Abstract"
-;   (para "An understanding of program evaluation can be a truly enlightening thing for a programmer. By breaking down the process to its essence and implementing the parsing and interpretation steps for a Lisp, we will explore both the interesting and oddly simple algorithms employed in making a language.")
-;   (para "Our Lisp will have familiar characteristics like variables, higher-order functions, conditionals, and lexical scope."))
+(slide
+  #:title "What are we talking about?"
+  (p "We're going to be talking about programming languages."))
 
 (slide
-  #:title "What are we talking about and why are we talking about it?"
-  (p #<<P
-We're going to be talking about programming languages. More specifically, we're
-going to be talking about interpreters. And even more specifically than that,
-we're going to talk about how you take a sequence of characters that only a
-human could understand and make a computer do something.
-P
-))
+  #:title "What are we talking about?"
+  (p "More specifically, we're going to be talking about interpreters."))
+
+(slide
+  #:title "What are we talking about?"
+  (p "And even more specifically than that, we're going to talk about how one
+     can take a sequence of characters that only a human could understand and
+     make a computer understand them."))
+
+(slide
+  #:title "And why would we talk about that?"
+  (p "Well, we're Software Engineers and as Software Engineers we write a lot
+     of code."))
+
+(slide
+  #:title "And why would we talk about that?"
+  (p "And how do we write that code? Well, with programming languages."))
+
+(slide
+  #:title "And why would we talk about that?"
+  (p "Programming languages are tools. Can you think of a tool that you use
+     more often? Most likely no."))
+
+(slide
+  #:title "And why would we talk about that?"
+  (p "An understanding of programming languages and their implementation, even
+     at a high level, will help you improve as a developer. Even if these
+     skills are not used every day, the knowledge will stay with you and help
+     you throughout your career."))
+
+(slide
+  #:title "So what are we going to do about it?")
+
+(slide
+  #:title "So what are we going to do about it?"
+  #:layout 'center
+  (t "Let's build an interpreter."))
+
+(slide
+  #:title "Where do we start?"
+  (p "How about with fancy buzzwords?"))
+
+(slide
+  (item "Parsers")
+  (item "Grammars")
+  (item "BNF/EBNF")
+  (item "Parser generators")
+  (item "Recursive descent parsers")
+  (item "Top down parser")
+  (item "Evaluation")
+  (item "Application")
+  (item "Scope"))
 
 ; (slide
-;   #:title "Scala Code"
-;   (mono #<<EOL
-; "cdr" -> Builtin({ (args, env) =>
-;   safeEval(args, env) match {
-;     case SExpr(_ :: tail) :: Nil =>
-;       SExpr(tail)
-;
-;     case Pair(_, tail) :: Nil => tail
-;     case _ :: Nil => Error(...)
-;     case _ => Error(...)
-;   }
-; })
-; EOL
-; ))
+;   (scale (bitmap (build-path "assets" "interpreter-definitions.png")) .5))
 ;
 ; (slide
-;   #:title "Lisp Code"
+;   #:title "Let's go back to that definition"
+;   (it "a program that can analyze and execute a program line by line."))
+;
+; (slide
+;   #:title "A program that can analyze a program"
 ;   (mono #<<CODE
-; (define -
-;   (lambda (. xs)
-;     (cond
-;       ((equal? 0 (length xs)) 0)
-;       ((equal? 1 (length xs))
-;        (* -1 (car xs)))
-;       (#t (fold (car xs)
-;                 sub
-;                 (cdr xs))))))
+; > 21 * 2
+; < 42
 ; CODE
 ; ))
