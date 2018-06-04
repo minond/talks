90a24501685c4d12ed30f6aab9a7c21e3149a3e8
Marcos Minond <minond.marcos@gmail.com>
2018-06-03 21:56:48 -0600

    slideshow.rkt

--- presentation.rkt
+++ presentation.rkt
@@ -23,45 +23,14 @@
 ; - Evaluator
 ;   - Pattern matching in Scala
 
-(require slideshow/code)
-(require slideshow/text)
+(require
+  slideshow/code
+  slideshow/text
+  "slideshow.rkt")
 
 (current-main-font "Baskerville")
 (current-code-font "IBM Plex Mono")
 (code-colorize-enabled #f)
-
-(define (unmargin line)
-  (let ([parts (string-split line "|")])
-    (string-trim (if (= 1 (length parts))
-                   (car parts)
-                   (car (cdr parts))))))
-
-(define (mono texts)
-  (apply vl-append 1
-         (for/list ([line (string-split (string-trim texts) "\n")])
-           (with-size ((get-current-code-font-size))
-                      (with-font (current-code-font)
-                                 (para #:fill? #f
-                                       #:width (current-para-width)
-                                       #:align 'left
-                                       line))))))
-
-(define (ordered . items)
-  (apply vl-append gap-size
-         (for/list ([x items]
-                    [n (range 1 (+ 1 (length items)))])
-           (item
-             #:gap-size 0
-             #:bullet (t (format "~s - " n))
-             x))))
-
-(define (unrdered . items)
-  (apply vl-append gap-size
-         (for/list ([x items])
-           (item x))))
-
-(define (p txt)
-  (para (string-normalize-spaces txt)))
 
 (slide
   #:layout 'center
---
+++ slideshow.rkt
@@ -0,0 +1,45 @@
+#lang slideshow
+
+(provide
+  unmargin
+  mono
+  ordered
+  unrdered
+  p)
+
+(require
+  slideshow/code
+  slideshow/text)
+
+(define (unmargin line)
+  (let ([parts (string-split line "|")])
+    (string-trim (if (= 1 (length parts))
+                   (car parts)
+                   (car (cdr parts))))))
+
+(define (mono texts)
+  (apply vl-append 1
+         (for/list ([line (string-split (string-trim texts) "\n")])
+           (with-size ((get-current-code-font-size))
+                      (with-font (current-code-font)
+                                 (para #:fill? #f
+                                       #:width (current-para-width)
+                                       #:align 'left
+                                       line))))))
+
+(define (ordered . items)
+  (apply vl-append gap-size
+         (for/list ([x items]
+                    [n (range 1 (+ 1 (length items)))])
+           (item
+             #:gap-size 0
+             #:bullet (t (format "~s - " n))
+             x))))
+
+(define (unrdered . items)
+  (apply vl-append gap-size
+         (for/list ([x items])
+           (item x))))
+
+(define (p txt)
+  (para (string-normalize-spaces txt)))
