e60122f02cb5488ec71ed2ee7f8eff70246c873c
Marcos Minond <minond.marcos@gmail.com>
2018-06-04 00:24:59 -0600

    typed expression with basic evaluator

--- language.scala
+++ language.scala
@@ -1,14 +1,50 @@
+/* Grammar:
+ *
+ * main    = { exprs } ;
+ * number  = [ "-" ] , digit { digit } ;
+ * digit   = 0 ... 9 ;
+ * string  = '"' , { letter } , '"' ;
+ * letter  = "A" ... "z" ;
+ * boolean = "#t" | "#f" ;
+ * identifier = identifier ,
+ *         { letter | symbol | digit } ;
+ * symbol  = "<" | ">" | "*" | "+" | "-"
+ *         | "=" | "_" | "/" | "%" ;
+ * atom    = identifier | number
+ *         | boolean ;
+ * exprs   = [ "'" ] ( atom | sexpr ) ;
+ * sexpr   = "(" { exprs } ")" ;
+ */
+
 sealed trait Token
-case class Number(value: Double) extends Token
-case class Str(value: String) extends Token
+case object SingleQuote extends Token
 case object OpenParen extends Token
 case object CloseParen extends Token
-case object True extends Token
-case object False extends Token
-case class Identifier(value: String) extends Token
+
+sealed trait Expr extends Token
+case class Number(value: Double) extends Expr
+case class Str(value: String) extends Expr
+case object True extends Expr
+case object False extends Expr
+case class Identifier(value: String) extends Expr
+case class SExpr(values: List[Expr]) extends Expr
+case class Quote(value: Expr) extends Expr
+case class Lambda(args: List[Identifier], body: Expr) extends Expr
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println("S")
+    val sum = SExpr(List(Identifier("+"), Number(32), Number(43)))
+    println(sum)
+    println(eval(sum))
+  }
+
+  def eval(expr: Expr): Expr = {
+    expr match {
+      case SExpr(sexpr) =>
+        sexpr match {
+          case Identifier("+") :: Number(a) :: Number(b) :: Nil =>
+            Number(a + b)
+        }
+    }
   }
 }
