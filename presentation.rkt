ed81c4a0cbc328e183d8113d7cea0a4fe465b228
Marcos Minond <minond.marcos@gmail.com>
2018-06-04 21:44:11 -0600

    combining some files

--- presentation.rkt
+++ presentation.rkt
@@ -25,12 +25,44 @@
 
 (require
   slideshow/code
-  slideshow/text
-  "slideshow.rkt")
+  slideshow/text)
 
 (current-main-font "Baskerville")
 (current-code-font "IBM Plex Mono")
 (code-colorize-enabled #f)
+
+(define (unmargin line)
+  (let ([parts (string-split line "|")])
+    (string-trim (if (= 1 (length parts))
+                   (car parts)
+                   (car (cdr parts))))))
+
+(define (mono texts #:ratio [ratio 1])
+  (apply vl-append 1
+         (for/list ([line (string-split (string-trim texts) "\n")])
+           (with-size ((get-current-code-font-size))
+                      (with-font (current-code-font)
+                                 (para #:fill? #f
+                                       #:width (* ratio (current-para-width))
+                                       #:align 'left
+                                       line))))))
+
+(define (ordered . items)
+  (apply vl-append gap-size
+         (for/list ([x items]
+                    [n (range 1 (+ 1 (length items)))])
+           (item
+             #:gap-size 0
+             #:bullet (t (format "~s - " n))
+             x))))
+
+(define (unordered . items)
+  (apply vl-append gap-size
+         (for/list ([x items])
+           (item x))))
+
+(define (p txt)
+  (para (string-normalize-spaces txt)))
 
 (slide
   #:layout 'center
