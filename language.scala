5d940953d1242c9ed64391a1e7da14917c0d9103
Marcos Minond <minond.marcos@gmail.com>
2018-06-04 21:40:03 -0600

    not combinator

--- language.scala
+++ language.scala
@@ -52,15 +52,16 @@ object Main {
   def passLambdas(expr: Expr): Expr =
     expr match {
       case SExpr(Identifier("lambda") :: SExpr(args) :: body :: Nil) =>
-        val (params, errs) = args.foldRight[(List[Identifier], List[InvalidExpr])](List.empty, List.empty){
-          case (curr, (params, errs)) =>
-            curr match {
-              case id @ Identifier(_) => (id :: params, errs)
-              case x =>
-                val msg = s"expecting identifier in lambda argument but got $x"
-                (params, InvalidExpr(msg) :: errs)
-            }
-        }
+        val (params, errs) =
+          args.foldRight[(List[Identifier], List[InvalidExpr])](List.empty, List.empty) {
+            case (curr, (params, errs)) =>
+              curr match {
+                case id @ Identifier(_) => (id :: params, errs)
+                case x =>
+                  val msg = s"expecting identifier in lambda argument but got $x"
+                  (params, InvalidExpr(msg) :: errs)
+              }
+          }
 
         if (!errs.isEmpty) errs(0)
         else Lambda(params, body)
@@ -71,8 +72,8 @@ object Main {
   def parse(ts: Iterator[Token]): Expr = {
     val tokens = ts.buffered
     passErrors(passLambdas(tokens.next match {
-      case expr @ (True | False | _: Str | _: Number | _: Identifier
-          | _: SExpr | _: Quote | _: Lambda | _: InvalidExpr) =>
+      case expr @ (True | False | _: Str | _: Number | _: Identifier | _: SExpr |
+          _: Quote | _: Lambda | _: InvalidExpr) =>
         expr.asInstanceOf[Expr]
 
       case SingleQuote =>
@@ -104,23 +105,27 @@ object Main {
     val src = str.toList.toIterator.buffered
 
     for (c <- src if !c.isWhitespace)
-      yield c match {
-        case '(' => OpenParen
-        case ')' => CloseParen
-        case '\'' => SingleQuote
-        case '"' => Str(consumeUntil(src, is('"')).mkString)
-        case n if isDigit(n) => Number((n + consumeWhile(src, isDigit).mkString).toDouble)
-        case c if isIdentifier(c) => Identifier(c + consumeWhile(src, isIdentifier).mkString)
+      yield
+        c match {
+          case '(' => OpenParen
+          case ')' => CloseParen
+          case '\'' => SingleQuote
+          case '"' => Str(src.takeWhile(not(is('"'))).mkString)
+          case n if isDigit(n) =>
+            Number((n + consumeWhile(src, isDigit).mkString).toDouble)
+          case c if isIdentifier(c) =>
+            Identifier(c + consumeWhile(src, isIdentifier).mkString)
 
-        case '#' => src.headOption match {
-          case Some('f') => src.next; False
-          case Some('t') => src.next; True
-          case Some(c) => src.next; InvalidToken(s"#$c")
-          case None => InvalidToken("#")
+          case '#' =>
+            src.headOption match {
+              case Some('f') => src.next; False
+              case Some('t') => src.next; True
+              case Some(c) => src.next; InvalidToken(s"#$c")
+              case None => InvalidToken("#")
+            }
+
+          case c => InvalidToken(c + consumeWhile[Char](src, isWord).mkString)
         }
-
-        case c => InvalidToken(c + consumeWhile[Char](src, isWord).mkString)
-      }
   }
 
   def consumeWhile[T](src: BufferedIterator[T], predicate: Prediate[T]): Iterator[T] = {
@@ -133,9 +138,6 @@ object Main {
 
     aux(List.empty).toIterator
   }
-
-  def consumeUntil[T](src: BufferedIterator[T], predicate: Prediate[T]): Iterator[T] =
-    src.takeWhile({ c => !predicate(c) })
 
   def isIdentifier(c: Char): Boolean =
     isDigit(c) || isLetter(c) || isSymbol(c)
@@ -157,4 +159,7 @@ object Main {
 
   def is[T](x: T): Prediate[T] =
     (y: T) => x == y
+
+  def not[T](f: Prediate[T]): Prediate[T] =
+    (x: T) => !f(x)
 }
