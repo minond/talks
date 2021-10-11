29e16ee6e7a411098a84824df4d4a02e33cfafca
Marcos Minond <marcos.minond@mx.com>
2021-10-11 14:01:45 -0600

    Ruby for cpp engs start

---
+++ ruby-for-cpp-engs/slides.rkt
@@ -0,0 +1,17 @@
+#lang slideshow
+
+(require
+  slideshow/code
+  slideshow/text)
+
+
+(current-main-font "Baskerville")
+(current-code-font "IBM Plex Mono")
+(code-colorize-enabled #f)
+
+
+(slide
+  #:layout 'center
+  (titlet "An introduction to Ruby")
+  (t "For C++ engineers"))
+
